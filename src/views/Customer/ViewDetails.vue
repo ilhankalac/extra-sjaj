cee0e6be7246098a4835d3dd62d8a81dd938d9a6
ilhankalac <ilhandeveloper@gmail.com>
2021-02-15 21:15:36 +0100

    customers details changes to tailwind code

--- src/views/Customer/ViewDetails.vue
+++ src/views/Customer/ViewDetails.vue
@@ -1,5 +1,5 @@
 <template>
-  <div :class="{ card: !payed, notpayed: payed }">
+  <div :class="{ 'bg-red-600 round p-5 rounded-lg text-white text-2xl': !payed, 'bg-green-500 round p-5 rounded-lg text-white text-2xl': payed }">
     <div class="container">
       <h3>
         <b>TOTAL: {{ total.toFixed(2) }} €</b> <br />
@@ -18,35 +18,4 @@ export default {
 };
 </script>
 
-<style scoped>
-.card {
-  /* Add shadows to create the "card" effect */
-  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
-  transition: 0.3s;
-  background: crimson;
-  color: #f8f8ff;
-  border-radius: 10px;
-  width: 250px;
-  margin-right: 20px;
-}
-
-.notpayed {
-  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
-  transition: 0.3s;
-  background: green;
-  color: #f8f8ff;
-  border-radius: 10px;
-  width: 250px;
-  margin-right: 20px;
-}
-
-/* On mouse-over, add a deeper shadow */
-.card:hover {
-  box-shadow: 0 8px 16px 0 rgba(0, 0, 0, 0.2);
-}
-
-/* Add some padding inside the card container */
-.container {
-  padding: 2px 16px;
-}
-</style>
+<style></style>
