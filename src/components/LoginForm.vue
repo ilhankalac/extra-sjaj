6e4a0c23d26d985df5cbb8783176fa9fd1aa9a92
ilhankalac <ilhandeveloper@gmail.com>
2021-02-15 20:56:23 +0100

    login page, shadow on form and more

--- src/components/LoginForm.vue
+++ src/components/LoginForm.vue
@@ -1,11 +1,18 @@
 <template>
-  <form @submit.prevent="handleSubmit" class="mx-20 text-2xl  flex flex-col my-60 sm:mx-10 lg:mx-80 gap-10 ">
-    <p class="text-red-800 text-4xl font-medium">Extra Sjaj - servis za pranje tepiha</p>
-    <input type="email" required placeholder="Email" v-model="email" />
-    <input type="password" required placeholder="Lozinka" v-model="password" />
-    <div class="error">{{ error }}</div>
-    <button class="bg-red-500 hover:bg-red-600 h-16 text-white">Prijavi se</button>
-  </form>
+  <div>
+    <form
+      @submit.prevent="handleSubmit"
+      class="mx-30 text-2xl  flex flex-col my-60 sm:mx-10 lg:mx-80 gap-10 
+      shadow-2xl border-red-500 p-5 
+    "
+    >
+      <p class="text-red-800 lg:text-4xl sm:text-2xl font-medium">Extra Sjaj - servis za pranje tepiha</p>
+      <input type="email" required placeholder="Email" v-model="email" />
+      <input type="password" required placeholder="Lozinka" v-model="password" />
+      <div class="error">{{ error }}</div>
+      <button class="bg-red-500  hover:bg-red-600 h-16 text-white ">Prijavi se</button>
+    </form>
+  </div>
 </template>
 
 <script>
